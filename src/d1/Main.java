3c7699749a82b2bfe049e3ea1302fedf5cdf81af
jason <18815726365@163.COM>
2021-04-06 10:30:04 +0800

    instanceof

--- src/d1/Main.java
+++ src/d1/Main.java
@@ -1,4 +1,22 @@
 package d1;
 
-public class Main {
+public class Main{
+    public static void main(String[] args) {
+        A a = null;
+        System.out.println(a instanceof A); //false
+
+        a = new B();
+        System.out.println(a instanceof A); //true
+
+        B b = new C();
+        System.out.println(b instanceof A); // true
+
+        C c = new C();
+        System.out.println(c instanceof A); //true
+
+    }
 }
+
+interface A {}
+class B implements A{}
+class C extends B{}
